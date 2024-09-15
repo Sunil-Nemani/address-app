21673698c239a89223551104dd388ddfed6a8025
suniln <suniln@integramicro.co.in>
2024-09-15 16:29:12 +0530

    rest template

--- src/main/java/com/sunil/address/config/AddressBeanInitializer.java
+++ src/main/java/com/sunil/address/config/AddressBeanInitializer.java
@@ -8,7 +8,9 @@ package com.sunil.address.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AddressBeanInitializer
@@ -16,10 +18,10 @@ public class AddressBeanInitializer
 	
 	private static final Logger log = LoggerFactory.getLogger(AddressBeanInitializer.class);
 	
-//	@Bean
-//	public RestTemplate getRestTemplate()
-//	{
-//		log.info("initializing RestTemplate");
-//		return new RestTemplate();
-//	}
+	@Bean
+	public RestTemplate getRestTemplate()
+	{
+		log.info("initializing RestTemplate");
+		return new RestTemplate();
+	}
 }
